52dc19722da8e4e9ad56dfe1f1ebe965dbdf4709
Cantalapiedra <gargamelle@gmail.com>
2020-03-03 21:28:48 +0100

    download release actually does that

--- swissprot_parser/download_release.sh
+++ swissprot_parser/download_release.sh
@@ -6,16 +6,9 @@ release="$1";
 UNIPROT_FTP=$(grep "^UNIPROT_FTP" config | cut -d $'\t' -f 2);
 UNIPROT_RELNOTES=$(grep "^UNIPROT_RELNOTES" config | cut -d $'\t' -f 2);
 UNIPROT_CURRENT=$(grep "^UNIPROT_CURRENT" config | cut -d $'\t' -f 2);
-
-echo "Downloading $UNIPROT_RELNOTES from $UNIPROT_FTP ...";
-
-ncftpget -v "$UNIPROT_FTP" ./ "$UNIPROT_RELNOTES";
+UNIPROT_PREVREL=$(grep "^UNIPROT_PREVREL" config | cut -d $'\t' -f 2);
 
 ##
-
-RELNOTES=$(basename "$UNIPROT_RELNOTES");
-
-#release=$(cat "$RELNOTES" | head -1 | cut -d $' ' -f 3);
 
 echo "Release to download: $release";
 
@@ -33,7 +26,7 @@ fi;
 
 echo "Preparing to download uniprot $release to $RELEASES_DIR";
 
-ncftpget -R "$UNIPROT_FTP" "$RELEASES_DIR"/"$release" "$UNIPROT_CURRENT"/uniprot_sprot.dat.gz
+ncftpget -R "$UNIPROT_FTP" "$RELEASES_DIR"/"$release" "$UNIPROT_PREVREL"/"release-${release}"/"knowledgebase"/"uniprot_sprot-only${release}.tar.gz"
 
 if [ $? -eq 0 ]; then
 	PREV_RELEASES_LIST=$(grep "^PREV_RELEASES_LIST" config | cut -d $'\t' -f 2);
