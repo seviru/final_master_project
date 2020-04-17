dcf59168c2924bb63e1fa6aa99253a2c6f948ad8
Cantalapiedra <cpcantalapiedra@gmail.com>
2020-04-17 16:41:18 +0200

    settings and docstrings

--- cluster_info_finder/cluster_finder.py
+++ cluster_info_finder/cluster_finder.py
@@ -1,17 +1,43 @@
-###############################################################
-### Script to find all clusters with more than 10 unigenes, ###
-### retrieve their name and store It on a file              ###
-###############################################################
+#!/usr/bin/env python3
 
+# Changed to PEP8 docstring
+"""Retrieve clusters (GMGC families) from MongoDB
+
+Script to find all clusters (GMGC families) 
+with minimum specified size (i.e. number of members or unigenes), 
+retrieve their name and store it on a file.
+
+Also, we will store only those clusters with hit to SwissProt.
+"""
+
+# TODO: the next comment should be elsewhere
+# since how the user loads python3 (by path, module, venv or conda)
+# is not a concern of the actual script
+#
 # MANUALLY DO THIS: module load Python/3.7.2-GCCcore-8.2.0
-from pymongo import MongoClient
-client = MongoClient("fat01", 27017)
 
-query = list(client.gmgc_clusters.members.find({"nu": {"$gte": 3}}, {"_id": 0, "cl": 1}))
+__all__ = [] # no API
+__author__ = "seviru"
+# check https://pep8.org/#module-level-dunder-names
+
 import sys
+from pymongo import MongoClient
+
+from .settings import MONGO_HOST, MONGO_PORT, CL_MIN_SIZE
+
+# Constants
+
+# TODO: capitalize
+# https://www.python.org/dev/peps/pep-0008/#constants
 partition_size = 10000      # Number of clusters we want to have on each partition
 partition_number = 0    # Number of partition we are writing in
 partition_counter = 0   # Partition counter to check we don't put more than needed files on each folder
+
+# Main
+
+client = MongoClient(MONGO_HOST, MONGO_PORT)
+
+query = list(client.gmgc_clusters.members.find({"nu": {"$gte": CL_MIN_SIZE}}, {"_id": 0, "cl": 1}))
 
 for cluster in query: # We just want to store the cluster which have at least 1 unigene with an exact or best hit in swissprot
     unigenes_list = client.gmgc_clusters.members.find_one({"cl": cluster["cl"]}, {"_id": 0, "clm": 1})["clm"]
@@ -25,6 +51,7 @@ for cluster in query: # We just want to store the cluster which have at least 1 
 
             try:    # Try to retrieve best swissprot hit
                 client.gmgc_unigenes.sprot_best.find_one({"u": unigene}, {"_id": 0, "spb":1})["spb"]["n"]
+                # TODO: filter by minimum evalue, score or identity
                 partition_counter += 1
                 sys.stdout.write(f"{cluster['cl']}\t{partition_number}\n")
                 break # If the cluster has at least 1 hit of sprot_best we save the cluster and check the next one
@@ -33,3 +60,5 @@ for cluster in query: # We just want to store the cluster which have at least 1 
     if partition_counter == partition_size:
         partition_number += 1
         partition_counter = 0
+
+## END
---
+++ cluster_info_finder/settings.py
@@ -0,0 +1,16 @@
+##
+## CPCantalapiedra 2020
+
+"""Default settings
+
+This file should be configured and renamed to "settings.py".
+MONGO_HOST and MONGO_PORT are required to access the MongoDB.
+CL_MIN_SIZE could be any positive integer (or 0), which is
+the minimum size of the clusters to retrieve.
+"""
+
+MONGO_HOST = "fat01"
+MONGO_PORT = 27017
+CL_MIN_SIZE = 3
+
+## END
