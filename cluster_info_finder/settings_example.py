2bdf8b99fd847ce0d684cfb93fd76a6f3c030506
Cantalapiedra <cpcantalapiedra@gmail.com>
2020-04-17 17:06:13 +0200

    settings_example

--- cluster_info_finder/settings_example.py
+++ cluster_info_finder/settings_example.py
@@ -1,5 +1,6 @@
 ##
-## CPCantalapiedra 2020
+
+__author__ "CPCantalapiedra 2020"
 
 """Default settings
 
@@ -9,8 +10,8 @@ CL_MIN_SIZE could be any positive integer (or 0), which is
 the minimum size of the clusters to retrieve.
 """
 
-MONGO_HOST = "fat01"
-MONGO_PORT = 27017
-CL_MIN_SIZE = 3
+MONGO_HOST = None # Set here the IP or host name to MongoDB
+MONGO_PORT = None # Set here the port to MongoDB
+CL_MIN_SIZE = 3 # Set here the minimum size (number of members) of the clusters to retrieve
 
 ## END
